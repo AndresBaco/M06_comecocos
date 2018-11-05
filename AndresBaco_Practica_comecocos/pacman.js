44befacf673c51b04a6b557a8ffba4c3e38e7ea8
AndresBaco <43445646+AndresBaco@users.noreply.github.com>
2018-11-05 19:22:55 +0100

    Add files via upload

---
+++ AndresBaco_Practica_comecocos/pacman.js
@@ -0,0 +1,158 @@
+var tauler = [ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			   [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+			   [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+			   [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+			   [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+			   [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+			   [0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+			   [0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+			   [0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0],
+			   [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+			   [0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0],
+			   [0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0],
+			   [0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0],
+			   [0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0],
+			   [0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0],
+			   [0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0],
+			   [0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0],
+			   [0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0],
+			   [0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+			   [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+			   [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+			   [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+			   [0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0],
+			   [0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+			   [0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+			   [0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0],
+			   [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+			   [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+			   [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+			   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+
+var Fantasma1= "X";
+var Fantasma2= "X";
+var Fantasma3= "X";
+var Jugador= "Q";
+
+function posicioInicialFantasmes(f1, f2, f3){
+	var fantasma=f1
+	var posicions= new Array()
+	for (i=0; i<3; i++)
+	{
+		while(true)
+		{
+			var numFiles= tauler.length
+			var numColumnes= tauler[0].length
+			var fila= Math.floor(Math.random()*numFiles)
+			var columna= Math.floor(Math.random()*numColumnes)
+			if (tauler[fila][columna] == 0 || tauler[fila][columna] == "X")
+			{
+				continue
+			}
+			else
+			{
+				tauler[fila][columna]=fantasma
+				break
+			}
+		}
+		if (fantasma ==f1)
+		{
+			fantasma=f2
+		}
+		else
+		{
+			fantasma = f3
+		}
+		
+	}
+	
+}
+
+function posicioInicialJugador(j){
+	while(true)
+		{
+			var numFiles= tauler.length
+			var numColumnes= tauler[0].length
+			var fila= Math.floor(Math.random()*numFiles)
+			var columna= Math.floor(Math.random()*numColumnes)
+			if (tauler[fila][columna] == 0 || tauler[fila][columna] == "X")
+			{
+				continue
+			}
+			else
+			{
+				tauler[fila][columna]=j
+				pos = [fila, columna]
+				return pos
+			}
+		}
+}
+
+
+function mostrarTaulell(taulell){
+	for(y=0;y<30;y++)
+	{	
+		for(x=0;x<28;x++)
+		{
+			if (tauler[y][x] != 0 && tauler[y][x] != 1)
+			{
+				document.write("<strong>"+tauler[y][x]+"</strong>")
+				document.write(" ")
+			}
+			else
+			{
+			document.write(tauler[y][x])
+			document.write(" ")
+			}
+			
+		}
+		document.write("<br>")
+	}
+}
+
+	
+function movimentJugador(e, x, y){
+	if (e == "ArrowDown")
+	{
+		console.log("Down")
+		tauler[y][x] = 1
+		y +=1
+		main(x, y)
+	}
+	else if (e=="ArrowUp")
+	{
+		console.log("up")
+		tauler[y][x] = 1
+		posicio[y]  -=1
+		main(x, y)
+	}
+	else if (e=="ArrowRight")
+	{
+		console.log("right")
+		tauler[y][x] = 1
+		posicio[x] +=1
+		main(x, y)
+	}
+	else if (e=="ArrowLeft")
+	{
+		console.log("left")
+		tauler[y][x] = 1
+		posicio[x] -=1
+		main(x,y)
+	}
+}
+
+
+function main(x){
+	mostrarTaulell();
+	document.write(x)
+	x += 1
+}
+
+posicioInicialFantasmes(Fantasma1, Fantasma2, Fantasma3)
+var posicioInicial=posicioInicialJugador(Jugador)
+var x=1
+setInterval(main(x), 1000)                                                
+
+
+
